fceb332c1227709a277f743351c5e21247f3704f
Ariel Alcaraz Checa <alcarazsincuenta@gmail.com>
2025-07-03 03:21:09 -0300

    Se agrego validacion para actualizar un producto

--- src/services/products.service.js
+++ src/services/products.service.js
@@ -1,9 +1,7 @@
 import * as z from 'zod/v4';
 
 const productsSchema = z.object({
-    name: z.string({
-        required_error: 'name: no puede estar vacio'
-    }),
+    name: z.string(),
     price: z.number().positive(),
     category: z.string(),
     stock: z.number().int().positive(),
@@ -12,4 +10,8 @@ const productsSchema = z.object({
 
 export const validationProduct = (valid) => {
     return productsSchema.safeParse(valid);
+}
+
+export const validationPartialProduct = (valid) => {
+    return productsSchema.partial().safeParse(valid);
 }
