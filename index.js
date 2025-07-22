7b70152f151bd1ee2d7b671a8b0fa1032b08703a
Ariel Alcaraz Checa <alcarazsincuenta@gmail.com>
2025-07-21 22:03:54 -0300

    udpate index.js

--- index.js
+++ index.js
@@ -10,18 +10,18 @@ import { fileURLToPath } from 'url';
 const app = express();
 const PORT = process.env.PORT ?? 3001;
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = Path2D.dirname(__filename);
+const __dirname = path.dirname(__filename);
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors())
 
-app.use('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'));
-});
 app.use('/api/products', productsRouter); //Ruta de productos
 app.use('/api/users', authentication, usersRouter); //Ruta de usuarios
 app.use('/auth', authRouter); //Ruta registro y login. Todavía en construcción
+app.use('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './src/public/index.html'));
+});
 
 //Puerto de servidor
 app.listen(PORT, () => {
