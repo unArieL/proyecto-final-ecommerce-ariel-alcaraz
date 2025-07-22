1dde75b7480d5c14ecdfe12d890177620ae23c47
Ariel Alcaraz Checa <alcarazsincuenta@gmail.com>
2025-07-22 06:06:59 -0300

    update

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -4,7 +4,7 @@ import 'dotenv/config';
 const SK = process.env.JWT_SECRET;
 
 export const authentication = (req, res, next) => {
-    const token = req.headers["authorization"].split(' ')[1];
+    const token = req.headers['authorization'].split(' ')[1];
 
     if (!token) {
         return res.sendStatus(401);
